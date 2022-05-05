d64eef0c4b7f68ba74311ff70749ac805d263b9b
Damien Vesper <ldamienvesper@gmail.com>
2022-05-04 23:53:41 -0500

    rect sprite

--- src/sprites/Rect.ts
+++ src/sprites/Rect.ts
@@ -1,5 +1,51 @@
 import Sprite from './Sprite';
 
-class Rect extends Sprite {}
+interface RectOpts {
+    id?: string
+
+    x: number
+    y: number
+
+    width: number,
+    height: number
+
+    fill?: string;
+    stroke?: {
+        color: string
+        width: number
+    }
+}
+
+class Rect extends Sprite {
+    width: number;
+    height: number;
+
+    fill: RectOpts[`fill`];
+    stroke: RectOpts[`stroke`];
+
+    constructor (opts: RectOpts) {
+        super(opts.id);
+
+        this.width = opts.width;
+        this.height = opts.height;
+
+        this.fill = opts.fill;
+        this.stroke = opts.stroke;
+    }
+
+    public readonly draw = (context: CanvasRenderingContext2D): void => {
+        if (this.fill !== undefined) {
+            context.fillStyle = this.fill;
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+
+        if (this.stroke !== undefined) {
+            context.strokeStyle = this.stroke.color;
+            context.lineWidth = this.stroke.width * this.scale.width;
+
+            context.strokeRect(this.x, this.y, this.width, this.height);
+        }
+    };
+}
 
 export default Rect;
